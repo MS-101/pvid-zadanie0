fe1a896ec65533ef3b8630165a2667ab7c6289d2
xsvab@stuba.sk <martin.svab@gmail.com>
2024-02-22 21:23:21 +0100

    finished training task

--- xsvab-pvid-zadanie0/src/main.cpp
+++ xsvab-pvid-zadanie0/src/main.cpp
@@ -1,96 +1,159 @@
 #include <iostream>
 #include <opencv2/opencv.hpp>
 
-cv::Mat preprocessImage(cv::Mat image) {
-	cv::Mat grayscale;
-	cv::cvtColor(image, grayscale, cv::COLOR_BGR2GRAY);
-
-	cv::Mat median;
-	cv::medianBlur(grayscale, median, 7);
-
-	return median;
-}
-
-cv::Mat binarizeImage(cv::Mat image) {
-	cv::Mat threshold;
-	cv::threshold(image, threshold, 110, 255, cv::THRESH_BINARY);
-
-	return threshold;
-}
-
-std::vector<std::vector<cv::Point>> getContours(cv::Mat image) {
-	std::vector<std::vector<cv::Point>> contours;
-	cv::findContours(image, contours, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_NONE);
-
-	return contours;
-}
-
-bool isContourCircular(const std::vector<cv::Point>& contour) {
-	double epsilon = 0.02 * cv::arcLength(contour, true);
-	std::vector<cv::Point> approx;
-	cv::approxPolyDP(contour, approx, epsilon, true);
-
-	double circularity = 0.0;
-	if (approx.size() >= 5) {
-		cv::RotatedRect ellipse = cv::fitEllipse(approx);
-		double area = cv::contourArea(contour);
-		double ellipseArea = CV_PI * ellipse.size.width * ellipse.size.height / 4.0;
-		circularity = area / ellipseArea;
-	}
-
-	return circularity > 0.6;
-}
 
 void countOranges() {
-	cv::Mat image = cv::imread("assets\\oranges.jpg");
-	cv::Mat preprocessed = preprocessImage(image);
-	cv::Mat binarized = binarizeImage(preprocessed);
-	std::vector<std::vector<cv::Point>> contours = getContours(binarized);
+	// input
 
+	cv::Mat input = cv::imread("assets\\oranges.jpg");
+	cv::imshow("input", input);
+
+	// preprocessing
+
+	cv::Mat grayscale;
+	cv::cvtColor(input, grayscale, cv::COLOR_BGR2GRAY);
+	cv::imshow("grayscale", grayscale);
+
+	// binarization
+
+	cv::Mat threshold;
+	cv::threshold(grayscale, threshold, 0, 255, cv::THRESH_OTSU);
+	cv::imshow("threshold", threshold);
+
+	// postprocessing
+
+	cv::Mat erode;
+	cv::Mat kernel = cv::getStructuringElement(cv::MORPH_ELLIPSE, cv::Size(30, 30));
+	cv::erode(threshold, erode, kernel);
+	cv::imshow("erode", erode);
+
+	// contour detection
+
+	cv::Mat output = input.clone();
 	int objectCount = 0;
-	cv::Mat output = binarized;
-	output.setTo(0);
+
+	std::vector<std::vector<cv::Point>> contours;
+	cv::findContours(erode, contours, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE);
 	
 	for (int i = 0; i < contours.size(); ++i) {
-		if (cv::contourArea(contours[i]) > 10000 && isContourCircular(contours[i])) {
-			cv::drawContours(output, contours, i, cv::Scalar(255), -1);
+		std::vector<cv::Point> points = contours[i];
+
+		if (cv::contourArea(points) > 100) {
 			objectCount++;
+
+			double centerX = 0.0;
+			double centerY = 0.0;
+
+			for (cv::Point point : points) {
+				centerX += point.x;
+				centerY += point.y;
+			}
+
+			centerX /= points.size();
+			centerY /= points.size();
+
+			int fontFace = cv::FONT_HERSHEY_PLAIN;
+			int fontScale = 1;
+
+			cv::Point fontPoint = cv::Point(centerX, centerY);
+			cv::Scalar color = cv::Scalar(0, 255, 0);
+
+			cv::putText(output, std::to_string(objectCount), fontPoint, fontFace, fontScale, color);
+			cv::drawContours(output, contours, i, color);
 		}
 	}
 
-	cv::imshow("Oranges", output);
-	std::cout << "Detected " << objectCount << " oranges." << std::endl;
+	// output
 
+	cv::imshow("output", output);
+	std::cout << "Detected " << objectCount << " oranges." << std::endl;
 	cv::waitKey();
 }
 
 void countBananas() {
-	cv::Mat image = cv::imread("assets\\bananas.jpg");
-	cv::Mat preprocessed = preprocessImage(image);
-	cv::Mat binarized = binarizeImage(preprocessed);
-	std::vector<std::vector<cv::Point>> contours = getContours(binarized);
+	// input
+	
+	cv::Mat input = cv::imread("assets\\bananas.jpg");
+	cv::imshow("input", input);
 
+	// preprocessing
+
+	cv::Mat grayscale;
+	cv::cvtColor(input, grayscale, cv::COLOR_BGR2GRAY);
+	cv::imshow("grayscale", grayscale);
+
+	// binarization
+
+	cv::Mat canny;
+	cv::Canny(grayscale, canny, 50, 100);
+	cv::imshow("canny", canny);
+
+	// postprocessing
+
+	cv::Mat dilate;
+	cv::Mat kernel = cv::getStructuringElement(cv::MORPH_ELLIPSE, cv::Size(10, 10));
+	cv::dilate(canny, dilate, kernel);
+	cv::imshow("dilate", dilate);
+
+	cv::Mat bananas;
+	cv::bitwise_not(dilate, bananas);
+	cv::imshow("bananas", bananas);
+
+	cv::Mat background;
+	cv::threshold(grayscale, background, 240, 255, cv::THRESH_BINARY);
+	cv::imshow("background", background);
+	
+	cv::Mat bananasWithoutBackground = bananas.clone();
+	bananasWithoutBackground.setTo(0, background);
+	cv::imshow("bananas without background", bananasWithoutBackground);
+
+	// contour detection
+
+	cv::Mat output = input.clone();
 	int objectCount = 0;
-	cv::Mat output = binarized;
-	output.setTo(0);
+
+	std::vector<std::vector<cv::Point>> contours;
+	cv::findContours(bananas, contours, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE);
 
 	for (int i = 0; i < contours.size(); ++i) {
-		if (cv::contourArea(contours[i]) > 10000) {
-			cv::drawContours(output, contours, i, cv::Scalar(255), -1);
+		std::vector<cv::Point> points = contours[i];
+
+		if (cv::contourArea(points) > 100) {
 			objectCount++;
+
+			double centerX = 0.0;
+			double centerY = 0.0;
+
+			for (cv::Point point : points) {
+				centerX += point.x;
+				centerY += point.y;
+			}
+
+			centerX /= points.size();
+			centerY /= points.size();
+
+			int fontFace = cv::FONT_HERSHEY_PLAIN;
+			int fontScale = 1;
+
+			cv::Point fontPoint = cv::Point(centerX, centerY);
+			cv::Scalar color = cv::Scalar(0, 0, 255);
+
+			cv::putText(output, std::to_string(objectCount), fontPoint, fontFace, fontScale, color);
+			cv::drawContours(output, contours, i, color);
 		}
 	}
 
-	cv::imshow("Bananas", output);
-	std::cout << "Detected " << objectCount << " bananas." << std::endl;
+	// output
 
+	cv::imshow("output", output);
+	std::cout << "Detected " << objectCount << " bananas." << std::endl;
 	cv::waitKey();
 }
 
 int main()
 {
 	countOranges();
-	//countBananas();
+	countBananas();
 
 	return 0;
 }
